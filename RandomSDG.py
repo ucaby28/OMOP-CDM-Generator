5c0d03889fd3a9fcfc2358a8c8c288f5687df5fc
Steven Lin <58918543+ucaby28@users.noreply.github.com>
2020-07-03 13:20:16 +0100

    Initial commit

---
+++ RandomSDG.py
@@ -0,0 +1,32 @@
+import csv
+from faker import Faker
+import datetime as dt
+
+
+def datagenerate(records, headers):
+    this_year = dt.datetime.today().year
+    fake = Faker('en_GB')
+    fake1 = Faker('en_US')
+
+    with open("Patient_data.csv", 'wt') as csvFile:
+        writer = csv.DictWriter(csvFile, fieldnames=headers)
+        writer.writeheader()
+
+        for i in range(records):
+            sdg_dob = fake1.date_time_ad()
+            writer.writerow({
+                "Patient Name": fake.name(),
+                "BIRTH_DATETIME": sdg_dob,
+                "Age": this_year - sdg_dob.year,
+                "Phone Number": fake.phone_number(),
+                "Address": fake.address(),
+                "Zip Code": fake.postcode(),
+                "City": fake.city(),
+            })
+
+
+records = 10000
+headers = ["Patient Name", "BIRTH_DATETIME", "Age", "Phone Number",
+           "Address", "Zip Code", "City"]
+datagenerate(records, headers)
+print("CSV generation complete!")
